b5b8f9d37bad4c74545d4149194a873be076a38d
AhmedTitef <57777850+AhmedTitef@users.noreply.github.com>
2020-12-15 02:17:12 -0800

    added firebase realtime database integration for both ios and android

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,33 @@
+import 'dart:io' ;
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gtaxi_app/screens/mainpage.dart';
 
-void main() {
-  runApp(MyApp());
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final FirebaseApp app = await Firebase.initializeApp(
+    name: 'db2',
+    options: Platform.isIOS || Platform.isMacOS
+        ? FirebaseOptions(
+      appId: '1:297855924061:ios:c6de2b69b03a5be8',
+      apiKey: 'AIzaSyCPVSNS03mulp7ZO-5FXtu6lv9ETU2jshY',
+      projectId: 'flutter-firebase-plugins',
+      messagingSenderId: '297855924061',
+      databaseURL: 'https://gtaxi-de48c-default-rtdb.firebaseio.com',
+    )
+        : FirebaseOptions(
+      appId: '1:678222681147:android:ba50fa6df415954848381e',
+      apiKey: 'AIzaSyA5YMoap1vXN4vmihYc7-n0W3CF9pO3M30',
+      messagingSenderId: '297855924061',
+      projectId: 'flutter-firebase-plugins',
+      databaseURL: 'https://gtaxi-de48c-default-rtdb.firebaseio.com',
+    ),
+  );
+runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
